37eba7f3d13f6c8617d98f67e758ffc5ef3b1a76
Raymundo Gutiérrez <129213631+RaymundoGtz@users.noreply.github.com>
2024-04-15 05:13:33 -0400

    Update MainActivity.kt

--- MainACT/MainActivity.kt
+++ MainACT/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -23,10 +26,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.iest.misdatos.ui.theme.MisDatosTheme
 import com.iest.misdatos.viewmodels.PreferencesViweModel
 import kotlinx.coroutines.launch
+import java.time.format.TextStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -74,43 +81,67 @@ fun SettingsView(preferencesViewModel: PreferencesViweModel){
 
         }
 
+        var weight by rememberSaveable {
+            mutableStateOf("")
 
-        var saved = true
+        }
+
+
+
+
+
         var corrutinasScope = rememberCoroutineScope()
         var savedAge = preferencesViewModel.age.collectAsState(initial = 0)
         var savedName = preferencesViewModel.name.collectAsState(initial = "???")
         var savedHobby = preferencesViewModel.hobby.collectAsState(initial = "???")
         var savedHeight = preferencesViewModel.height.collectAsState(initial = 0)
+        var savedWeight= preferencesViewModel.weight.collectAsState(initial = 0)
 
-        Column (Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally){
+        val saved = if (savedName.value == "???") {
+            false
+        } else {
+            true
+        }
+
+
+
+        Column (Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
 
             if(saved){
 
-                Text(text = "Tu nombre es ${savedName.value}, tienes ${savedAge.value}, tu hobby es ${savedHobby.value}  y tu altura es ${savedHeight.value} cm")
+                Text(modifier = Modifier.padding(start = 25.dp, top = 20.dp, end = 25.dp), textAlign = TextAlign.Center, fontSize = 20.sp,text = "Tu nombre es ${savedName.value}, tienes ${savedAge.value}, tu hobby es ${savedHobby.value}, mides ${savedHeight.value} cm y pesas ${savedWeight.value}kg")
 
 
             }else{
 
-                TextField(label = {Text(text = "MaferSex")}, value = name, onValueChange = {
+                Text(modifier = Modifier
+                    .padding(bottom = 20.dp), fontSize = 20.sp, text = "Introduce tus datos")
+
+                TextField(label = {Text(text = "Nombre")}, value = name, onValueChange = {
                     name = it
                 })
 
-                TextField(value = age, onValueChange = {
+                TextField(label = { Text(text = "Edad")}, value = age, onValueChange = {
                     age = it
                 })
 
-                TextField(value = hobby, onValueChange = {
+                TextField(label = { Text(text = "Hobby")}, value = hobby, onValueChange = {
                     hobby = it
                 })
 
-                TextField(value = height, onValueChange = {
+                TextField(label = { Text(text = "Altura")}, value = height, onValueChange = {
                     height = it
                 })
-                Button(onClick = {
+
+                TextField(label = { Text(text = "Peso")}, value = weight, onValueChange = {
+                    weight = it
+                })
+
+                Button(modifier = Modifier.padding(top = 20.dp), onClick = {
                     corrutinasScope.launch {
 
                         if(age.toIntOrNull() != null){
-                            preferencesViewModel.setNameAndAgeAndHobby(name, age.toInt(), hobby, height.toInt())
+                            preferencesViewModel.setNameAndAgeAndHobby(name, age.toInt(), hobby, height.toInt(), weight.toInt())
                         }
 
                     }
@@ -120,14 +151,12 @@ fun SettingsView(preferencesViewModel: PreferencesViweModel){
                 }
 
             }
-
             
-                Text(text = "Tu nombre es ${savedName.value}, tienes ${savedAge.value}, tu hobby es ${savedHobby.value}  y tu altura es ${savedHeight.value} cm")
-
         }
 
 
 
     }
 }
+
 
